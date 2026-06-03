65b684cf32dc0481a339a0934fb73f8a4da9b8b0
chelsea-zh <chelseazh2025@gmail.com>
2026-06-02 17:16:14 -0700

    partially functioning tasklist

--- src/ToDo.tsx
+++ src/ToDo.tsx
@@ -1,5 +1,15 @@
-let daily = false
-let dailyTasks = []
+import './ToDo.css'
+
+let daily = true
+
+// task = {reward: 100, goal: "string", claimed: false}
+
+let dailyTasks = [
+    {reward: 100, goal: "testing", claimed: true},
+    {reward: 200, goal: "other test", claimed: false},
+    {reward: 100, goal: "test 3", claimed: true},
+    {reward: 200, goal: "test 4", claimed: false}
+]
 let customTasks = []
 
 export default function ToDo() {
@@ -12,26 +22,87 @@ export default function ToDo() {
             ) : (
                 <Custom />
             )}
+            <ToDoSelector />
+            <ToDoSelector />
         </div>
     )
 }
 
-function ToDoSelector() {}
+function ToDoSelector() {
+    return(
+        <div className="sel">
+            <h1>s</h1>
+        </div>
+    )
+}
 
 function Dailies() {
     return(
-       <h1>dailies</h1>
+        <div className="dailies">
+            <h1>dailies</h1>
+            <TaskList tasks = {dailyTasks} />
+        </div>
     )
 }
 
 function Custom() {
     return(
-       <h1>custom</h1>
+       <h1 className="custom">custom</h1>
     )
 }
 
-function TaskLisk() {}
+function TaskList({tasks}) {
+    return(
+        <div className='tasklist'>
+            <UnclaimedTasks tasks = {tasks} />
+            <ClaimedTasks tasks = {tasks} />
+        </div>
+    )
+}
 
-function Task() {}
+function UnclaimedTasks({tasks}) {
+    let tasklist = [];
+    tasks.forEach((t) => {
+        if (!t.claimed) {
+            tasklist.push(
+                <Task task = {t}/>
+            )
+        }
+    });
+
+    return(
+        <div className='unclaimed'>
+            {tasklist}
+        </div>
+    )
+}
+
+function ClaimedTasks({tasks}) {
+    let tasklist = [];
+    tasks.forEach((t) => {
+        if (t.claimed) {
+            tasklist.push(
+                <Task task = {t}/>
+            )
+        }
+    });
+
+    return(
+        <div className='claimed'>
+            {tasklist}
+        </div>
+    )
+}
+
+function Task({task}) {
+    return(
+        <div className="task">
+            <h1> {task.reward} </h1>
+            <h1> {task.goal} </h1>
+            <button> claim </button>
+            <button> delete </button>
+        </div>
+    )
+}
 
 function TaskCreator() {}
